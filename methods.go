a5bc5a25bd9e614852814fb616b8ea3091ee54df
Kelvin Fichter <kef50@case.edu>
2018-03-21 14:50:40 -0400

    Updated whitelist

--- methods.go
+++ methods.go
@@ -7,146 +7,126 @@ type method struct {
 
 var rpcMethods = map[string]method{
 	// == Blockchain ==
-	"callcontract":          {true},
-	"getaccountinfo":        {true},
-	"getbestblockhash":      {true},
-	"getblock":              {true},
-	"getblockchaininfo":     {true},
-	"getblockcount":         {true},
-	"getblockhash":          {true},
-	"getblockheader":        {true},
-	"getchaintips":          {true},
-	"getdifficulty":         {true},
-	"getmempoolancestors":   {true},
+	"callcontract": {true},
+	"getaccountinfo": {true},
+	"getbestblockhash": {true},
+	"getblock": {true},
+	"getblockchaininfo": {true},
+	"getblockcount": {true},
+	"getblockhash": {true},
+	"getblockheader": {true},
+	"getchaintips": {true},
+	"getdifficulty": {true},
+	"getmempoolancestors": {true},
 	"getmempooldescendants": {true},
-	"getmempoolentry":       {true},
-	"getmempoolinfo":        {true},
-	"getrawmempool":         {true},
+	"getmempoolentry": {true},
+	"getmempoolinfo": {true},
+	"getrawmempool": {true},
 	"gettransactionreceipt": {true},
-	"gettxout":              {true},
-	"gettxoutproof":         {true},
-	"gettxoutsetinfo":       {true},
-	"listcontracts":         {true},
-	// preciousblock "blockhash"
-	// pruneblockchain
-	// verifychain ( checklevel nblocks )
+	"gettxout": {true},
+	"gettxoutproof": {true},
+	"gettxoutsetinfo": {true},
+	"listcontracts": {true},
+	"preciousblock": {true},
+	"pruneblockchain": {true},
+	"verifychain": {true},
 	"verifytxoutproof": {true},
 
 	// == Control ==
-	// getinfo
 	"getinfo": {true},
-	// getmemoryinfo
-	// help ( "command" )
-	// stop
+	"getinfo": {true},
+	"getmemoryinfo": {true},
+	"help": {true},
+	"stop": {true}
 
 	// == Generating ==
 	"generate": {true},
-	// generatetoaddress nblocks address (maxtries)
+	"generatetoaddress": {true},
 
 	// == Mining ==
-	// getblocktemplate ( TemplateRequest )
-	// getmininginfo
-	// getnetworkhashps ( nblocks height )
-	// getstakinginfo
-	// getsubsidy [nTarget]
-	// prioritisetransaction <txid> <priority delta> <fee delta>
-	// submitblock "hexdata" ( "jsonparametersobject" )
+	"getblocktemplate": {true},
+	"getmininginfo": {true},
+	"getnetworkhashps": {true},
+	"getstakinginfo": {true},
+	"getsubsidy": {true},
+	"prioritisetransaction": {true},
+	"submitblock": {true},
 
 	// == Network ==
-	// addnode "node" "add|remove|onetry"
-	// clearbanned
-	// disconnectnode "node"
-	// getaddednodeinfo ( "node" )
-	// getconnectioncount
-	// getnettotals
-	// getnetworkinfo
-	// getpeerinfo
-	// listbanned
-	// ping
-	// setban "subnet" "add|remove" (bantime) (absolute)
-	// setnetworkactive true|false
+	"addnode": {true},
+	"clearbanned": {true},
+	"disconnectnode": {true},
+	"getaddednodeinfo": {true},
+	"getconnectioncount": {true},
+	"getnettotals": {true},
+	"getnetworkinfo": {true},
+	"getpeerinfo": {true},
+	"listbanned": {true},
+	"ping": {true},
+	"setban": {true},
+	"setnetworkactive": {true},
 
 	// == Rawtransactions ==
-	// createrawtransaction [{"txid":"id","vout":n},...] {"address":amount,"data":"hex",...} ( locktime )
-	// decoderawtransaction "hexstring"
-	// decodescript "hexstring"
-	// fromhexaddress "hexaddress"
-	// fundrawtransaction "hexstring" ( options )
-	// gethexaddress "address"
-	// getrawtransaction "txid" ( verbose )
-	// sendrawtransaction "hexstring" ( allowhighfees )
-	// signrawtransaction "hexstring" ( [{"txid":"id","vout":n,"scriptPubKey":"hex","redeemScript":"hex"},...] ["privatekey1",...] sighashtype )
+	"createrawtransaction": {true},
+	"decoderawtransaction": {true},
+	"decodescript": {true},
+	"fromhexaddress": {true},
+	"fundrawtransaction": {true},
+	"gethexaddress": {true},
+	"getrawtransaction": {true},
+	"sendrawtransaction": {true},
+	"signrawtransaction": {true},
 
 	// == Util ==
-	// createmultisig nrequired ["key",...]
-	// estimatefee nblocks
-	// estimatepriority nblocks
-	// estimatesmartfee nblocks
-	// estimatesmartpriority nblocks
-	// signmessagewithprivkey "privkey" "message"
-	// validateaddress "address"
-	// verifymessage "address" "signature" "message"
+	"createmultisig": {true},
+	"estimatefee": {true},
+	"estimatepriority": {true},
+	"estimatesmartfee": {true},
+	"estimatesmartpriority": {true},
+	"signmessagewithprivkey": {true},
+	"validateaddress": {true},
+	"verifymessage": {true},
 
 	// == Wallet ==
-	// abandontransaction "txid"
-	// addmultisigaddress nrequired ["key",...] ( "account" )
-	// addwitnessaddress "address"
-	// backupwallet "destination"
-	// bumpfee "txid" ( options )
-	// createcontract "bytecode" (gaslimit gasprice "senderaddress" broadcast)
-	"createcontract": {false},
-	// dumpprivkey "address"
-	// dumpwallet "filename"
-	// encryptwallet "passphrase"
-	// getaccount "address"
+	"abandontransaction": {true},
+	"addmultisigaddress": {true},
+	"addwitnessaddress": {true},
+	"backupwallet": {true},
+	"bumpfee": {true},
+	"createcontract": {true},
+	"dumpprivkey": {true},
+	"dumpwallet": {true},
+	"encryptwallet": {true},
 	"getaccount": {true},
-	// DEPRECATED getaccountaddress "account"
-	// DEPRECATED getaddressesbyaccount "account"
-	// getbalance ( "account" minconf include_watchonly )
 	"getbalance": {true},
-	// getnewaddress ( "account" )
-	"getnewaddress": {false},
-	// getrawchangeaddress
-	// DEPRECATED getreceivedbyaccount "account" ( minconf )
-	// getreceivedbyaddress "address" ( minconf )
+	"getnewaddress": {true},
+	"getrawchangeaddress": {true},
 	"getreceivedbyaddress": {true},
-	// gettransaction "txid" ( include_watchonly )
 	"gettransaction": {true},
-	// getunconfirmedbalance
 	"getunconfirmedbalance": {true},
-	// getwalletinfo
 	"getwalletinfo": {true},
-	// importaddress "address" ( "label" rescan p2sh )
-	// importmulti "requests" "options"
-	// importprivkey "qtum" ( "label" ) ( rescan )
-	// importprunedfunds
-	// importpubkey "pubkey" ( "label" rescan )
-	// importwallet "filename"
-	// keypoolrefill ( newsize )
-	"keypoolrefill": {false},
-	// DEPRECATED listaccounts ( minconf include_watchonly)
-	// listaddressgroupings
-	// listlockunspent
-	// listreceivedbyaccount ( minconf include_empty include_watchonly)
-	// listreceivedbyaddress ( minconf include_empty include_watchonly)
-	// listsinceblock ( "blockhash" target_confirmations include_watchonly)
+	"importaddress": {true},
+	"importmulti": {true},
+	"importprivkey": {true},
+	"importprunedfunds": {true},
+	"importpubkey": {true},
+	"importwallet": {true},
+	"keypoolrefill": {true},
+	"listaddressgroupings": {true},
+	"listlockunspent": {true},
+	"listreceivedbyaccount": {true},
+	"listreceivedbyaddress": {true},
 	"listsinceblock": {true},
-	// listtransactions ( "account" count skip include_watchonly)
 	"listtransactions": {true},
 	"listunspent": {true},
-	// lockunspent unlock ([{"txid":"txid","vout":n},...])
-	// DEPRECATED move "fromaccount" "toaccount" amount ( minconf "comment" )
-	// removeprunedfunds "txid"
-	// reservebalance [<reserve> [amount]]
-	// DEPRECATED sendfrom "fromaccount" "toaddress" amount ( minconf "comment" "comment_to" )
-	// sendmany "fromaccount" {"address":amount,...} ( minconf "comment" ["address",...] )
-	// sendmanywithdupes "fromaccount" {"address":amount,...} ( minconf "comment" ["address",...] )
-	// sendtoaddress "address" amount ( "comment" "comment_to" subtractfeefromamount )
-	"sendtoaddress": {false},
-	// sendtocontract "contractaddress" "data" (amount gaslimit gasprice senderaddress broadcast)
-	"sendtocontract": {false},
-	// DEPRECATED. setaccount "address" "account"
-	// settxfee amount
-	// signmessage "address" "message"
+	"lockunspent": {true},
+	"removeprunedfunds": {true},
+	"reservebalance": {true},
+	"sendmany": {true},
+	"sendmanywithdupes": {true},
+	"sendtoaddress": {true},
+	"sendtocontract": {true},
+	"settxfee": {true},
+	"signmessage": {true},
 	"waitforlogs": {true},
 }
